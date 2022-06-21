585a89ad03a5f32bb64a9456b6e59c3ca21859c6
syaaazn <syaaazn@gmail.com>
2022-06-21 15:18:30 +0900

    add:C言語で一文字のみのRSA実装を行なった。これから文字列での実装を行う。

--- c/main.c
+++ c/main.c
@@ -4,32 +4,37 @@
 unsigned int gcd(unsigned int a, unsigned int b);
 unsigned int lcm(unsigned int a, unsigned int b);
 unsigned int modpow(unsigned int a, unsigned int e, unsigned int n);
-void keyGenerate(unsigned int *n,unsigned int * e,unsigned int *d);
-void encrypt(char *plain,unsigned int *encrypted_ascii, unsigned int n, unsigned int e);
+void keyGenerate(unsigned int *n, unsigned int *e, unsigned int *d);
+void encrypt(char *plain, unsigned int *encrypted_ascii, unsigned int n, unsigned int e);
 char *decrypt(unsigned int *encrypted_ascii, unsigned int n, unsigned int d);
 
-int main(){
+int main()
+{
+    printf("plain text : \n");
+    char plain[8];
+    scanf("%s", plain);
 
-    
-    /*unsigned int n,e,d;
-    keyGenerate(&n,&e,&d);
+    unsigned int n, e, d;
+    keyGenerate(&n, &e, &d);
     printf("generated keys : \n");
-    printf("n : %d, e : %e, d %d \n",n,e,d);
+    printf("n : %u, e : %u, d : %u \n", n, e, d);
+
     printf("encrypted asciicode :\n");
-    char[100] plain;
-    unsigned int[100] encrypted_ascii;
-    encrypt(plain,encrypted_ascii,n,e);
-    for (int i = 0; i < sizeof(encrypted_ascii)/sizeof(int); i++)                      //暗号化した平文を出力
+    unsigned int encrypted_ascii[8];
+    encrypt(plain, encrypted_ascii, n, e);
+    for (int i = 0; i < sizeof(encrypted_ascii) / sizeof(int); i++) //暗号化した平文を出力
     {
-        if (i >= (sizeof(encrypted_ascii))/sizeof(int) - 1)
+        if (i >= (sizeof(encrypted_ascii)) / sizeof(int) - 1)
         {
-            printf(" %d\n",encrypted_ascii[i]);
+            printf(" %d\n", encrypted_ascii[i]);
         }
         else
         {
-            printf("%d, ",encrypted_ascii[i]);
+            printf("%d, ", encrypted_ascii[i]);
         }
-    }*/
+    }
+    printf("decrypted text :\n");
+    printf("%s", decrypt(encrypted_ascii, n, d));
 
     return 0;
 }
@@ -66,40 +71,42 @@ unsigned int modpow(unsigned int a, unsigned int e, unsigned int n)
     return result;
 }
 
-void keyGenerate(unsigned int *n,unsigned int * e,unsigned int *d){
+void keyGenerate(unsigned int *n, unsigned int *e, unsigned int *d)
+{
     unsigned const int p = 13;              //指定されている素数
     unsigned const int q = 19;              //指定されている素数
-    *n = p * q;                 //公開鍵
+    *n = p * q;                             //公開鍵
     unsigned int euler = (p - 1) * (q - 1); // Euler(n) = (p - 1) * (q -1)の計算結果
-    *e = 2;                     //公開鍵の生成(Euler(n)と互いに素になるeが見つかるまでループさせる)
-    while (gcd(euler, *e) != 1)
+    unsigned int key = 2;                   //公開鍵の生成(Euler(n)と互いに素になるeが見つかるまでループさせる)
+    while (gcd(euler, key) != 1)
     {
-        *e++;
+        key++;
     }
-    *d = 2; //秘密鍵の生成((e * d) mod Euler(n) = 1 を満たすdが見つかるまでループさせる)
-    while (*e * *d % euler != 1)
+    *e = key;
+    key = 2; //秘密鍵の生成((e * d) mod Euler(n) = 1 を満たすdが見つかるまでループさせる)
+    while (*e * key % euler != 1)
     {
-        *d++;
+        key++;
     }
+    *d = key;
 }
 
-void encrypt(char* plain,unsigned int *encrypted_ascii, unsigned int n, unsigned int e)
+void encrypt(char *plain, unsigned int *encrypted_ascii, unsigned int n, unsigned int e)
 {
-    for (int i = 0; i < sizeof(plain)/sizeof(char); i++)
+    for (int i = 0; i < sizeof(*plain) / sizeof(char); i++)
     {
-        unsigned int ascii_trans = (int)(plain[i]); //平文をASCIIコードに変換
-        encrypted_ascii[i] = (modpow(ascii_trans, e, n));      //暗号化して配列に格納(M^eの処理は愚直に行うと桁溢れする懸念があるので別途modpow関数を利用している)
+        unsigned int ascii_trans = (int)(plain[i]);       //平文をASCIIコードに変換
+        encrypted_ascii[i] = (modpow(ascii_trans, e, n)); //暗号化して配列に格納(M^eの処理は愚直に行うと桁溢れする懸念があるので別途modpow関数を利用している)
     }
 }
 
 char *decrypt(unsigned int *encrypted_ascii, unsigned int n, unsigned int d)
 {
     char *decrypted_str;
-    for (int i = 0; i < sizeof(encrypted_ascii)/sizeof(int); i++)
+    for (int i = 0; i < sizeof(encrypted_ascii) / sizeof(int); i++)
     {
         unsigned int decrypted_ascii = modpow(encrypted_ascii[i], d, n); //復号化(C^dの処理は愚直に行うと桁溢れする懸念があるので別途modpow関数を利用している)
-        decrypted_str[i] = (char)decrypted_ascii;                  //文字に変換
+        decrypted_str[i] = (char)decrypted_ascii;                        //文字に変換
     }
     return decrypted_str;
 }
-
